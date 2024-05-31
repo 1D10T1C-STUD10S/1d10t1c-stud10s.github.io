54d0eb08196ef458ddf917c0f46d86f261bba360
Dev10us <112738649+melonmasteristaken@users.noreply.github.com>
2024-05-31 08:14:07 +0800

    Create blog.js

---
+++ js/blog.js
@@ -0,0 +1,18 @@
+ 
+document.addEventListener("DOMContentLoaded", function() {
+  var excerptElements = document.querySelectorAll('.post-excerpt');
+  excerptElements.forEach(function(excerptElement) {
+    var innerHTML = excerptElement.innerHTML.trim();
+    var tempContainer = document.createElement('div');
+    tempContainer.innerHTML = innerHTML;
+    var firstChild = tempContainer.firstElementChild;
+    if (firstChild && firstChild.tagName.toLowerCase() !== 'p') {
+      var pTag = document.createElement('p');
+      pTag.innerHTML = firstChild.innerHTML;
+      tempContainer.replaceChild(pTag, firstChild);
+      excerptElement.innerHTML = tempContainer.innerHTML;
+    }
+    var truncatedText = excerptElement.innerHTML.substring(0, 150) + '...';
+    excerptElement.innerHTML = truncatedText;
+  });
+});
